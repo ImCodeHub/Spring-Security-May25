78ea7bebe98146e55417bda034baccfddda841f6
Ankit <ankitsharma.as420@gmail.com>
2025-06-11 15:51:01 +0530

    added the json property to specify mapping to variable in json

--- src/main/java/com/example/Spring_Security_May25/Model/AuthenticationResponse.java
+++ src/main/java/com/example/Spring_Security_May25/Model/AuthenticationResponse.java
@@ -1,10 +1,12 @@
 package com.example.Spring_Security_May25.Model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class AuthenticationResponse {
+        @JsonProperty("access_token")
     public String accessToken;
 }
