cd9579fa0c104afc594fad959c266504f13890ae
Ankit <ankitsharma.as420@gmail.com>
2025-06-11 15:48:15 +0530

    added new login method

--- src/main/java/com/example/Spring_Security_May25/Service/UserService/UserDetails.java
+++ src/main/java/com/example/Spring_Security_May25/Service/UserService/UserDetails.java
@@ -1,12 +1,16 @@
 package com.example.Spring_Security_May25.Service.UserService;
 
 import com.example.Spring_Security_May25.Entity.User;
+import com.example.Spring_Security_May25.Model.AuthenticationRequest;
 import com.example.Spring_Security_May25.Model.AuthenticationResponse;
 import com.example.Spring_Security_May25.Model.RegisterRequest;
 import com.example.Spring_Security_May25.Repository.UserRepository;
 import com.example.Spring_Security_May25.Service.Utility.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,6 +23,12 @@ public class UserDetails {
     @Autowired
     private final JwtService jwtService;
 
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private final AuthenticationManager authenticationManager;
+
 
 //    method to register a User
     public AuthenticationResponse saveUser(RegisterRequest registerRequest){
@@ -26,11 +36,23 @@ public class UserDetails {
                 .firstName(registerRequest.getFirstName())
                 .lastName(registerRequest.getLastName())
                 .email(registerRequest.getEmail())
-                .password(registerRequest.getPassword())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
                 .role(registerRequest.getRole())
                 .build();
-
         userRepository.save(user);
+        String jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().accessToken(jwtToken).build();
+    }
+
+    // method to login in Account
+    public AuthenticationResponse Authentication(AuthenticationRequest authenticationRequest){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authenticationRequest.getUserName(),
+                        authenticationRequest.getPassword()
+                )
+        );
+        User user = userRepository.findByEmail(authenticationRequest.getUserName()).orElseThrow(() -> new RuntimeException());
         String jwtToken = jwtService.generateToken(user);
         return AuthenticationResponse.builder().accessToken(jwtToken).build();
     }
