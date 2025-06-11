e185fcf9db87677af11e1d313029532e1a76cb29
Ankit <ankitsharma.as420@gmail.com>
2025-06-11 15:52:13 +0530

    created new end point for logining

--- src/main/java/com/example/Spring_Security_May25/Controller/AuthController.java
+++ src/main/java/com/example/Spring_Security_May25/Controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.example.Spring_Security_May25.Controller;
 
+import com.example.Spring_Security_May25.Model.AuthenticationRequest;
 import com.example.Spring_Security_May25.Model.AuthenticationResponse;
 import com.example.Spring_Security_May25.Model.RegisterRequest;
 import com.example.Spring_Security_May25.Service.UserService.UserDetails;
@@ -7,11 +8,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+/// this is the public Accessible
+
+@CrossOrigin
 @RestController
 @RequestMapping("api/auth/v1")
 @RequiredArgsConstructor
@@ -25,4 +26,13 @@ public class AuthController {
         AuthenticationResponse authenticationResponse = userDetails.saveUser(registerRequest);
         return new ResponseEntity<>(authenticationResponse, HttpStatus.CREATED);
     }
+
+    @PostMapping("login")
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest authenticationRequest){
+        AuthenticationResponse authentication = userDetails.Authentication(authenticationRequest);
+        return new ResponseEntity<>(authentication, HttpStatus.OK);
+    }
+
+
+
 }
