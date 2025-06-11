630201ac1e11a38e6f982cbead99bf00e9c1a846
Ankit <ankitsharma.as420@gmail.com>
2025-06-11 15:49:04 +0530

    added security configuration and added the prefix of the API and given the Role access to the api.

--- src/main/java/com/example/Spring_Security_May25/Configuration/SecurityConfiguration.java
+++ src/main/java/com/example/Spring_Security_May25/Configuration/SecurityConfiguration.java
@@ -1,8 +1,50 @@
 package com.example.Spring_Security_May25.Configuration;
 
+import com.example.Spring_Security_May25.JwtFilter.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    //Injecting custom authentication provider for handling user credentials.
+    @Autowired
+    private final AuthenticationProvider authenticationProvider;
+
+    //injecting custom jwt filet to intercept request and valid tokens.
+    @Autowired
+    private final JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> req
+                        .requestMatchers("/api/auth/v1/**")
+                        .permitAll()
+                        .requestMatchers("/api/admin/v1/**").hasRole("ADMIN")
+                        .requestMatchers("/api/employee/v1/**").hasAnyRole("EMPLOYEE","ADMIN")
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 
 }
